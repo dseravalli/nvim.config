09b0056e1a15658a1e3d5aee66da9442c00f6541
Daniel Seravalli <daniel@holler.io>
2024-10-25 22:54:16 -0400

    TailwindSorter

--- lua/dseravalli/lazy/tailwindsorter.lua
+++ lua/dseravalli/lazy/tailwindsorter.lua
@@ -4,9 +4,8 @@ return {
   build = 'cd formatter && npm i && npm run build',
   config = function()
     require('tailwind-sorter').setup({
-      on_save_enabled = false,
       on_save_pattern = { '*.html', '*.js', '*.jsx', '*.tsx', '*.twig', '*.hbs', '*.php',
-        '*.heex', '*.astro' },
+        '*.heex', '*.astro', '*.svelte' },
     })
   end,
 }
