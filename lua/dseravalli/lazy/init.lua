f9ba11b985d5465be70d6a33b21f949f427b87bd
Daniel Seravalli <daniel@holler.io>
2024-10-28 18:56:33 -0400

    Plugin re-org

--- lua/dseravalli/lazy/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  "tpope/vim-rhubarb",
-  "szw/vim-maximizer",
-  "christoomey/vim-tmux-navigator",
-  { "numToStr/Comment.nvim", opts = {} },
-  -- "HiPhish/jinja.vim",
-  -- "github/copilot.vim",
-}
