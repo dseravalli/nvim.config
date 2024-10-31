c146f5b85c1a1229f7119182aa5af38de215dc1a
Daniel Seravalli <daniel@holler.io>
2024-10-31 12:46:10 -0400

    Remove a keymap

--- lua/dseravalli/lazy/lsp.lua
+++ lua/dseravalli/lazy/lsp.lua
@@ -194,7 +194,6 @@ return {
 
       vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
       vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-      vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
       vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format buffer' })
     end,
   },
