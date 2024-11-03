834d70668618f82191e18f59ec2d238d0812a8f6
Daniel Seravalli <daniel@holler.io>
2024-11-03 00:54:09 -0400

    Neotest and move bn/bp to ]b [b

--- lua/dseravalli/lazy/neotest.lua
+++ lua/dseravalli/lazy/neotest.lua
@@ -10,17 +10,22 @@ return {
   config = function()
     require("neotest").setup({
       adapters = {
-        require("neotest-python")
+        require("neotest-python")({
+          args = { "--verbose", "-rP" }
+        })
       }
     })
 
     vim.keymap.set('n', '<leader>tc', function() require('neotest').run.run() end, { desc = 'Run Nearest Test' })
+    vim.keymap.set('n', '<leader>tp', function() require('neotest').run.run_last() end, { desc = 'Run Prev Test' })
     vim.keymap.set('n', '<leader>tT', function() require('neotest').run.run(vim.uv.cwd()) end,
       { desc = 'Run All Test Files' })
     vim.keymap.set('n', '<leader>td', function() require('neotest').run.run({ strategy = "dap" }) end,
       { desc = 'Debug test' })
     vim.keymap.set('n', '<leader>to', function() require("neotest").output_panel.toggle() end,
-      { desc = 'Open test output panel' })
-    vim.keymap.set('n', '<leader>ts', function() require("neotest").summary.toggle() end, { desc = 'Show test output' })
+      { desc = 'Open test output' })
+    vim.keymap.set('n', '<leader>tO', function() require("neotest").output.open() end,
+      { desc = 'Show single test output' })
+    vim.keymap.set('n', '<leader>ts', function() require("neotest").summary.toggle() end, { desc = 'Show test summary' })
   end,
 }
--- lua/dseravalli/remap.lua
+++ lua/dseravalli/remap.lua
@@ -7,8 +7,6 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('v', '<leader>p', "\"_dP", { desc = "Paste without overwrite" })
-vim.keymap.set('n', 'bn', ':bn<CR>')
-vim.keymap.set('n', 'bp', ':bp<CR>')
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make file executable" })
 vim.keymap.set('n', '<leader>c', ':BufferKill<CR>', { desc = "Close buffer" })
 vim.keymap.set("n", "<leader>g", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
@@ -28,6 +26,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- center window after next/prev search result
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set('n', ']b', ':bn<CR>')
+vim.keymap.set('n', '[b', ':bp<CR>')
 
 vim.keymap.set("n", "]q", "<cmd>cnext<CR>zz", { desc = "Next quickfix" })
 vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz", { desc = "Prev quickfix" })
