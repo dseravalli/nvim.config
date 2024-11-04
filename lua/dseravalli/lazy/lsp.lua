205f6d9ec63246796a8cd5a71f02f2fc869804e1
Daniel Seravalli <daniel@holler.io>
2024-11-03 20:19:20 -0500

    remove keymaps not needed with Oil

--- lua/dseravalli/lazy/lsp.lua
+++ lua/dseravalli/lazy/lsp.lua
@@ -113,11 +113,6 @@ return {
 
         -- Lesser used LSP functionality
         nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-        nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-        nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-        nmap('<leader>wl', function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, '[W]orkspace [L]ist Folders')
       end
 
       require("fidget").setup({})
