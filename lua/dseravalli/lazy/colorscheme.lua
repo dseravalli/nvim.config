7f7f34618b95c3103e27974fa45d1c935cd8b946
Daniel Seravalli <danielseravalli@gmail.com>
2025-03-13 18:33:16 -0400

    x

--- lua/dseravalli/lazy/colorscheme.lua
+++ lua/dseravalli/lazy/colorscheme.lua
@@ -1,17 +1,18 @@
 return {
-  -- onedark
-  {
-    'navarasu/onedark.nvim',
-    lazy = true,
-  },
+	-- tokyonight
+	-- variations are tokyonight-night, tokyonight-day, tokyonight-storm, tokynight-moon
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("tokyonight-moon")
+		end,
+	},
 
-  -- tokyonight
-  -- variations are tokyonight-night, tokyonight-day, tokyonight-storm, tokynight-moon
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'tokyonight-moon'
-    end,
-  },
+	-- rosepine
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = true,
+	},
 }
