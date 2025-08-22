c81ff515e92e579fd22edd3033fdb2b69ab08aed
Daniel Seravalli <danielseravalli@gmail.com>
2025-08-23 05:10:49 +0700

    clear up some deprecations

--- lua/dseravalli/lazy/lsp.lua
+++ lua/dseravalli/lazy/lsp.lua
@@ -226,8 +226,8 @@ return {
 			end, 100)
 
 			-- Diagnostic keymaps
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+			vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, { desc = "Go to previous diagnostic message" })
+			vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, { desc = "Go to next diagnostic message" })
 		end,
 	},
 }
