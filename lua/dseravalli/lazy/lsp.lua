81940acc742187f493016de0ba4977404150249b
Daniel Seravalli <danielseravalli@gmail.com>
2025-08-24 16:24:35 +0700

    x

--- lua/dseravalli/lazy/lsp.lua
+++ lua/dseravalli/lazy/lsp.lua
@@ -217,13 +217,21 @@ return {
 				end)
 			end, 100)
 
+			-- Configure diagnostics
+			vim.diagnostic.config({
+				float = {
+					source = true,
+				},
+			})
+
 			-- Diagnostic keymaps
 			vim.keymap.set("n", "[d", function()
-				vim.diagnostic.jump({ count = -1 })
+				vim.diagnostic.jump({ count = -1, float = true })
 			end, { desc = "Go to previous diagnostic message" })
 			vim.keymap.set("n", "]d", function()
-				vim.diagnostic.jump({ count = 1 })
+				vim.diagnostic.jump({ count = 1, float = true })
 			end, { desc = "Go to next diagnostic message" })
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open diagnostic float window" })
 		end,
 	},
 }
