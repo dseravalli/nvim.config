1e7638e293d90d807279f3082e8917453769dfc8
Daniel Seravalli <danielseravalli@gmail.com>
2025-08-30 19:09:08 +0700

    Disable formatting from ruby-lsp

--- lsp/ruby.lua
+++ lsp/ruby.lua
@@ -1,15 +1,16 @@
 ---@type vim.lsp.Config
 return {
-	cmd = { "ruby-lsp" },
-	root_markers = { "Gemfile", ".git" },
-	filetypes = { "ruby" },
-	init_options = {
-		formatter = "standard",
-		linters = { "standard" },
-		addonSettings = {
-			["Ruby LSP Rails"] = {
-				enablePendingMigrationsPrompt = false,
-			},
-		},
-	},
+  cmd = { "ruby-lsp" },
+  root_markers = { "Gemfile", ".git" },
+  filetypes = { "ruby" },
+  init_options = {
+    enabledFeatures = {
+      formatting = false,
+    },
+    addonSettings = {
+      ["Ruby LSP Rails"] = {
+        enablePendingMigrationsPrompt = false,
+      },
+    },
+  },
 }
