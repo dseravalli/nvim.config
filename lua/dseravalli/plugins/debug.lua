3d480ca2d0acf2bf3ca8b1bfe4cb1501c47164a4
Daniel Seravalli <danielseravalli@gmail.com>
2025-11-02 15:39:18 -0500

    Redo debugger config

--- lua/dseravalli/plugins/debug.lua
+++ lua/dseravalli/plugins/debug.lua
@@ -31,91 +31,59 @@ return {
       },
     },
     dependencies = {
-      "rcarriga/nvim-dap-ui",
-      "williamboman/mason.nvim",
-      "jay-babu/mason-nvim-dap.nvim",
-      "leoluz/nvim-dap-go",
-      "mxsdev/nvim-dap-vscode-js",
-      "mfussenegger/nvim-dap-python",
       "nvim-neotest/nvim-nio",
+      "rcarriga/nvim-dap-ui",
+      "mfussenegger/nvim-dap-python",
     },
     config = function()
       local dap = require("dap")
       local dapui = require("dapui")
+      dapui.setup()
+
+      require("dap-python").setup("uv")
 
       vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
-
-      require("mason-nvim-dap").setup({
-        automatic_setup = true,
-
-        handlers = {},
-
-        ensure_installed = {
-          "python",
-          "delve",
-          "js-debug-adapter",
-        },
-      })
-
-      -- Dap UI setup
-      dapui.setup()
 
       dap.listeners.after.event_initialized["dapui_config"] = dapui.open
       dap.listeners.before.event_terminated["dapui_config"] = dapui.close
       dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
-      -- Install golang specific config
-      require("dap-go").setup()
+      local js_debugger_path = vim.fn.stdpath("data")
+        .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js"
 
-      -- Install python specific config
-      --
-      require("dap").configurations["python"] = {
-        {
-          type = "python",
-          request = "launch",
-          name = "Launch file (console = integratedTerminal, justMyCode = false)",
-          program = "${file}",
-          console = "integratedTerminal",
-          justMyCode = false,
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = { js_debugger_path, "${port}" },
         },
       }
 
-      -- Let dap-python auto-detect the Python interpreter
-      require("dap-python").setup(nil)
-
-      -- Install JS specific config
-      require("dap-vscode-js").setup({
-        -- Use Mason's installation path for js-debug-adapter
-        debugger_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter",
-        adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-      })
-
-      require("dap").configurations["javascript"] = {
+      dap.configurations.javascript = {
         {
           type = "pwa-node",
           request = "launch",
-          name = "Launch current file",
+          name = "Current JS file",
           program = "${file}",
           cwd = "${workspaceFolder}",
         },
       }
-      require("dap").configurations["typescript"] = {
+
+      dap.configurations.typescript = {
         {
           type = "pwa-node",
           request = "launch",
-          name = "Launch current file (Typescript)",
+          name = "Current JS file",
           program = "${file}",
           cwd = "${workspaceFolder}",
-          -- runtimeArgs = { '--loader=ts-node/esm' },
-          runtimeExecutable = "ts-node",
+          runtimeExecutable = "tsx",
           sourceMaps = true,
-          protocol = "inspector",
-          outFiles = { "${workspaceFolder}/**/**/*", "!**/node_modules/**" },
-          skipFiles = { "<node_internals>/**", "node_modules/**" },
-          resolveSourceMapLocations = {
-            "${workspaceFolder}/**",
-            "!**/node_modules/**",
-          },
+          smartStep = true,
+          console = "integratedTerminal",
+          internalConsoleOptions = "neverOpen",
+          skipFiles = { "<node_internals>/**", "${workspaceFolder}/node_modules/**" },
         },
       }
     end,
