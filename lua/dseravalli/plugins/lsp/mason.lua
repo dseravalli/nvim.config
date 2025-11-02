02f693f844dcb2312e4d9a82a11b2e191e6696bc
Daniel Seravalli <danielseravalli@gmail.com>
2025-11-02 14:34:21 -0500

    x

--- lua/dseravalli/plugins/lsp/mason.lua
+++ lua/dseravalli/plugins/lsp/mason.lua
@@ -4,5 +4,8 @@ return {
   priority = 1000,
   config = function()
     require("mason").setup()
+
+    local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
+    vim.env.PATH = mason_bin .. ":" .. vim.env.PATH
   end,
 }
