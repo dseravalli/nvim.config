bd9c9d0df3517c41e36df8b7946cb00839feee17
Daniel Seravalli <danielseravalli@gmail.com>
2026-04-01 02:07:44 -0400

    treesitter

--- lua/dseravalli/plugins/treesitter.lua
+++ lua/dseravalli/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   branch = "main",
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
@@ -8,23 +9,22 @@ return {
     "vrischmann/tree-sitter-templ",
   },
   config = function()
-    require("nvim-treesitter").setup({
-      ensure_installed = {
-        "c",
-        "cpp",
-        "go",
-        "lua",
-        "python",
-        "rust",
-        "ruby",
-        "tsx",
-        "javascript",
-        "typescript",
-        "vimdoc",
-        "vim",
-        "bash",
-        "terraform",
-      },
+    require("nvim-treesitter").setup({})
+    require("nvim-treesitter").install({
+      "c",
+      "cpp",
+      "go",
+      "lua",
+      "python",
+      "rust",
+      "ruby",
+      "tsx",
+      "javascript",
+      "typescript",
+      "vimdoc",
+      "vim",
+      "bash",
+      "terraform",
     })
   end,
 }
